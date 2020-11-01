5d2fa973ebcfd2dbb3d05351ee7a1afd07b0e858
chengw54 <wendy.cheng@mail.utoronto.ca>
2020-11-01 00:33:33 -0700

    event setter getter constructor

--- phase1/src/entities/Event.java
+++ phase1/src/entities/Event.java
@@ -1,4 +1,42 @@
 package src.entities;
 
+import java.util.List;
+
 public class Event {
+    private String title;
+    private int speakerId;
+    private int roomNumber;
+    private int startingTime;
+    private List<Integer> eventAttendees;
+
+    public Event(String title, int speakerId, int roomNumber, int startingTime){
+        this.title = title;
+        this.speakerId = speakerId;
+        this.roomNumber = roomNumber;
+        this.startingTime = startingTime;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public int getSpeakerId(){
+        return this.speakerId;
+    }
+
+    public int getRoomNumber(){
+        return this.roomNumber;
+    }
+
+    public int getStartingTime(){
+        return this.startingTime;
+    }
+
+    public List<Integer> getEventAttendees(){
+        return eventAttendees;
+    }
 }
