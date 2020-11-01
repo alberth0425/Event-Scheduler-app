49ed5586b0784fac3422ccbbfad9e6673d99c54d
ywz <w.ye@mail.utoronto.ca>
2020-10-31 23:48:41 -0700

    create 3 user class

--- phase1/src/entities/Attendee.java
+++ phase1/src/entities/Attendee.java
@@ -1,4 +1,5 @@
 package src.entities;
 
 public class Attendee extends User{
+
 }
--- phase1/src/entities/User.java
+++ phase1/src/entities/User.java
@@ -1,4 +1,17 @@
 package src.entities;
 
 public class User {
+
+    protected int id;
+    protected String password;
+    protected static int numUser;
+    protected String username;
+
+    public User(String username, String password){
+        this.username = username;
+        this.password = password;
+        numUser += 1;
+        this.id = numUser;
+    }
+
 }
