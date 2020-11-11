ae7d66db82c1ee156a054966ac6f3b9e7ed32440
Peter Ke <peterke0911@gmail.com>
2020-11-11 12:59:53 -0800

    Implement createUser and modifyCredentials for AuthService

--- phase1/src/entities/Savable.java
+++ phase1/src/entities/Savable.java
@@ -2,4 +2,14 @@ package src.entities;
 
 public interface Savable {
     String toSavableString();
+
+    /**
+     * Check that a string is savable in CSV format, i.e. it does not contain commas or newlines.
+     *
+     * @param string the string to be validated
+     * @return true if the string does not contain commas or newlines
+     */
+    static boolean isStringSavable(String string) {
+        return !(string.contains(",") || string.contains("\n"));
+    }
 }
--- phase1/src/use_cases/AuthService.java
+++ phase1/src/use_cases/AuthService.java
@@ -1,12 +1,127 @@
 package src.use_cases;
 
+import src.entities.Savable;
 import src.entities.User;
 
 import java.util.HashMap;
 
 public class AuthService {
+    private static final int PASSWORD_MINIMUM_LENGTH = 6;
+    
     HashMap<String, User> users = new HashMap<>();
     
     public AuthService() {
+    }
+
+    /**
+     * Create a new user with given username, password, first name, and last name.
+     * 
+     * @param username username of the user
+     * @param password password of the user
+     * @param firstName first name of the user
+     * @param lastName last name of the user
+     * @throws UsernameAlreadyTakenException if the username is already taken by another user
+     * @throws InvalidFieldException if one of the fields are invalid
+     */
+    public void createUser(String username, String password, String firstName, String lastName) throws AuthException {
+        // Validate user fields
+        if (!validateUsername(username)) {
+            throw new InvalidFieldException(UserField.USERNAME);
+        } else if (!validatePassword(password)) {
+            throw new InvalidFieldException(UserField.PASSWORD);
+        } else if (!Savable.isStringSavable(firstName)) {
+            throw new InvalidFieldException(UserField.FIRST_NAME);
+        } else if (!Savable.isStringSavable(lastName)) {
+            throw new InvalidFieldException(UserField.LAST_NAME);
+        }
+        
+        // Check whether username already exists
+        if (users.containsKey(username)) {
+            throw new UsernameAlreadyTakenException();
+        }
+        
+        // Create the new user
+        User user = new User(username, password, firstName, lastName);
+        users.put(username, user);
+    }
+    
+    /**
+     * Modify the login credentials of a user.
+     * 
+     * @param username the current username of the user whose credentials are to be modified
+     * @param newUsername the new username for the user
+     * @param newPassword the new password for the user
+     * @throws UserDoesNotExistException if the user with the specified username does not exist
+     * @throws UsernameAlreadyTakenException if the new username is already taken by another user
+     * @throws InvalidFieldException if either the new username or password is invalid
+     */
+    public void modifyCredentials(String username, String newUsername, String newPassword) throws AuthException {
+        // Check that the user with given username exists
+        if (!users.containsKey(username)) {
+            throw new UserDoesNotExistException();
+        }
+
+        // Check that the new username and password are valid
+        if (!validateUsername(newUsername)) {
+            throw new InvalidFieldException(UserField.USERNAME);
+        } else if (!validatePassword(newPassword)) {
+            throw new InvalidFieldException(UserField.PASSWORD);
+        }
+        
+        // Check whether the new username is taken, if it is different from the old username
+        if (users.containsKey(newUsername) && !username.equals(newUsername)) {
+            throw new UsernameAlreadyTakenException();
+        }
+        
+        // Update the user
+        User user = users.remove(username);
+        user.setUsername(newUsername);
+        user.setPassword(newPassword);
+        users.put(newUsername, user);
+    }
+
+    /**
+     * Validate username for a user.
+     * 
+     * @param username the username to be validated
+     * @return true if the username is valid, i.e. it is savable and is not empty
+     */
+    private static boolean validateUsername(String username) {
+        return Savable.isStringSavable(username) && username.length() > 0;
+    }
+    
+    /**
+     * Validate password for a user.
+     * 
+     * @param password the password to be validated
+     * @return true if the password is valid, i.e. it is savable and has length of at least PASSWORD_MINIMUM_LENGTH
+     */
+    private static boolean validatePassword(String password) {
+        return Savable.isStringSavable(password) && password.length() >= PASSWORD_MINIMUM_LENGTH;
+    }
+
+    // Custom Exception Classes
+    
+    public static class AuthException extends Exception {}
+    public static class UserDoesNotExistException extends AuthException {}
+    public static class UsernameAlreadyTakenException extends AuthException {}
+
+    public enum UserField {
+        USERNAME,
+        PASSWORD,
+        FIRST_NAME,
+        LAST_NAME,
+    }
+
+    public static class InvalidFieldException extends AuthException {
+        private final UserField field;
+
+        private InvalidFieldException(UserField field) {
+            this.field = field;
+        }
+
+        public UserField getField() {
+            return field;
+        }
     }
 }
