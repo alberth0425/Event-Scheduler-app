6822962dc93db44812c2bfd51e40b2030a9106f6
Peter Ke <peterke0911@gmail.com>
2020-11-10 20:25:37 -0800

    Add basic fields for AuthService

--- phase1/src/use_cases/AuthService.java
+++ phase1/src/use_cases/AuthService.java
@@ -1,4 +1,12 @@
 package src.use_cases;
 
+import src.entities.User;
+
+import java.util.HashMap;
+
 public class AuthService {
+    HashMap<String, User> users = new HashMap<>();
+    
+    public AuthService() {
+    }
 }
