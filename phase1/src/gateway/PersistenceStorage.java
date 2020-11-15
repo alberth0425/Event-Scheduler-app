53a3066e3f2f3a5492f4caa204f4ad1845cee326
Qiwen Hua <qiwen.hua@mail.utoronto.ca>
2020-11-14 21:27:10 -0800

    Changed storage files directory to avoid Markus error (no dir at top level)

--- phase1/src/gateway/PersistenceStorage.java
+++ phase1/src/gateway/PersistenceStorage.java
@@ -10,13 +10,13 @@ public class PersistenceStorage {
      * Path constants for different Savable classes
      */
 
-    public static final String ATTENDEE_STORAGE_PATH = "./storage/attendees.csv";
-    public static final String SPEAKER_STORAGE_PATH = "./storage/speakers.csv";
-    public static final String ORGANIZER_STORAGE_PATH = "./storage/organizers.csv";
-    public static final String EVENT_STORAGE_PATH = "./storage/events.csv";
-    public static final String ROOM_STORAGE_PATH = "./storage/rooms.csv";
-    public static final String MESSAGE_STORAGE_PATH = "./storage/messages.csv";
-    public static final String CONTACT_BOOK_PATH = "./storage/contact_book.csv";
+    public static final String ATTENDEE_STORAGE_PATH = "./phase1/storage/attendees.csv";
+    public static final String SPEAKER_STORAGE_PATH = "./phase1/storage/speakers.csv";
+    public static final String ORGANIZER_STORAGE_PATH = "./phase1/storage/organizers.csv";
+    public static final String EVENT_STORAGE_PATH = "./phase1/storage/events.csv";
+    public static final String ROOM_STORAGE_PATH = "./phase1/storage/rooms.csv";
+    public static final String MESSAGE_STORAGE_PATH = "./phase1/storage/messages.csv";
+    public static final String CONTACT_BOOK_PATH = "./phase1/storage/contact_book.csv";
 
     /**
      * Save input entries as a csv/txt file to the input path.
