38ca7f6601449e4597162d9f3425fa5275f5c1f3
Yufan Qian <lynn.qian@mail.utoronto.ca>
2020-11-19 14:02:27 -0500

    add javadoc to Attendee.java, Organizer.java, Speaker.java.

--- phase1/src/entities/Organizer.java
+++ phase1/src/entities/Organizer.java
@@ -4,10 +4,23 @@ import java.util.List;
 
 public class Organizer extends User {
 
+    /**
+     * the constructor for organizer class.
+     *
+     * @param username the username of this organizer
+     * @param password the password this organizer needs to login
+     * @param firstName the firstname of this organizer
+     * @param lastName the lastname of this organizer
+     */
     public Organizer(String username, String password, String firstName, String lastName) {
         super(username, password, firstName, lastName);
     }
 
+    /**
+     * construct organizer from a dataEntry.
+     *
+     * @param dataEntry the savable string that represents organizer
+     */
     public Organizer(String dataEntry) {
         super(dataEntry);
     }
